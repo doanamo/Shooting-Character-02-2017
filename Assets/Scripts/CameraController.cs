14c68f7576c71bc96ac39899c654f63f6474e16b
Piotr Doan <doanpiotr@gmail.com>
2017-02-18 16:28:24 +0100

    Implement a simple camera follow.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject m_camera;
+    public GameObject m_target;
+
+    public Vector3 m_cameraOffset;
+    public Vector3 m_targetOffset;
+
+    void LateUpdate()
+    {
+        // Calculate position and direction of the camera.
+        Vector3 targetPosition = m_target.transform.position + m_targetOffset;
+        Vector3 cameraPosition = targetPosition + m_cameraOffset;
+        Vector3 targetDirection = (targetPosition - cameraPosition).normalized;
+
+        m_camera.transform.position = cameraPosition;
+        m_camera.transform.rotation = Quaternion.LookRotation(targetDirection);
+    }
+}
