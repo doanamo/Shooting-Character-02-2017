e6346d52d44005ec69d94bba99e55b700e9a0f5b
Piotr Doan <doanpiotr@gmail.com>
2017-02-18 17:34:27 +0100

    Implement control of camera distance from a target.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,17 +7,33 @@ public class CameraController : MonoBehaviour
     public GameObject m_camera;
     public GameObject m_target;
 
-    public Vector3 m_cameraOffset;
-    public Vector3 m_targetOffset;
+    public float   m_cameraDistance;
+    public Vector3 m_cameraDirection;
+    public Vector3 m_targetAnchor;
+
+    public float m_minimumDistance;
+    public float m_maximumDistance;
+
+    void Update()
+    {
+        // Control camera distance using the mouse wheel.
+        // Todo: Implement proper smoothing with varying speed depending on the distance.
+        m_cameraDistance += Input.GetAxis("Mouse ScrollWheel") * -1000.0f * Time.deltaTime;
+        m_cameraDistance = Mathf.Clamp(m_cameraDistance, m_minimumDistance, m_maximumDistance);
+    }
 
     void LateUpdate()
     {
-        // Calculate position and direction of the camera.
-        Vector3 targetPosition = m_target.transform.position + m_targetOffset;
-        Vector3 cameraPosition = targetPosition + m_cameraOffset;
-        Vector3 targetDirection = (targetPosition - cameraPosition).normalized;
+        // Calculate position of the camera.
+        Vector3 targetPosition = m_target.transform.position + m_targetAnchor;
 
-        m_camera.transform.position = cameraPosition;
-        m_camera.transform.rotation = Quaternion.LookRotation(targetDirection);
+        m_camera.transform.position = targetPosition;
+        m_camera.transform.rotation = Quaternion.Euler(m_cameraDirection);
+        m_camera.transform.Translate(0.0f, m_cameraDistance, 0.0f);
+
+        // Rotate camera towards the target.
+        Vector3 targetOffset = targetPosition - m_camera.transform.position;
+
+        m_camera.transform.rotation = Quaternion.LookRotation(targetOffset.normalized);
     }
 }
