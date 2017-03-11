f2591b456453beb3faadcede7ec2784730c3800a
Piotr Doan <doanpiotr@gmail.com>
2017-03-11 08:42:56 +0100

    Implement character aiming state.

--- Assets/Scripts/CharacterLogic.cs
+++ Assets/Scripts/CharacterLogic.cs
@@ -14,6 +14,10 @@ public class CharacterLogic : MonoBehaviour
     private readonly int HashMovementX = Animator.StringToHash("Movement X");
     private readonly int HashMovementZ = Animator.StringToHash("Movement Z");
 
+    private readonly int HashAiming = Animator.StringToHash("Aiming");
+    private readonly int HashAimingX = Animator.StringToHash("Aiming X");
+    private readonly int HashAimingZ = Animator.StringToHash("Aiming Z");
+
     private void Start()
     {
         this.controller = GetComponent<CharacterController>();
@@ -23,14 +27,29 @@ public class CharacterLogic : MonoBehaviour
     public void Move(Vector3 direction)
     {
         // Set character's animator parameters.
-        this.animator.SetBool(HashMoving, direction != Vector3.zero);
+        this.animator.SetBool(this.HashMoving, direction != Vector3.zero);
         this.animator.SetFloat(this.HashMovementX, direction.x, 0.15f, Time.fixedDeltaTime);
         this.animator.SetFloat(this.HashMovementZ, direction.z, 0.15f, Time.fixedDeltaTime);
+    }
+
+    public void Aim(Vector3 direction)
+    {
+        // Set character's animator parameters.
+        this.animator.SetBool(this.HashAiming, direction != Vector3.zero);
+        this.animator.SetFloat(this.HashAimingX, direction.x, 0.0f, Time.fixedDeltaTime);
+        this.animator.SetFloat(this.HashAimingZ, direction.z, 0.0f, Time.fixedDeltaTime);
     }
 
     private void FixedUpdate()
     {
         // Apply gravity to the character controller.
         this.controller.Move(Physics.gravity * 0.1f * Time.fixedDeltaTime);
+    }
+
+    public void LateUpdate()
+    {
+        // Reset character's animator paremeters.
+        this.animator.SetBool(this.HashMoving, false);
+        this.animator.SetBool(this.HashAiming, false);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,40 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Create a movement vector from the input.
-        // Todo: Implement smooth direction change.
+        // Handle directional input for character movement.
         Vector3 movementDirection = Vector3.zero;
         movementDirection.x = Input.GetAxis("Horizontal");
         movementDirection.z = Input.GetAxis("Vertical");
         movementDirection.Normalize();
 
-        // Move the controlled character.
-        this.character.Move(movementDirection);
+        if(movementDirection != Vector3.zero)
+        {
+            // Move with the controlled character.
+            this.character.Move(movementDirection);
+        }
+
+        // Handle right mouse button input for character aiming.
+        if(Input.GetMouseButton(1))
+        {
+            // Create a plane positioned at the character's feet.
+            Plane plane = new Plane(new Vector3(0.0f, 1.0f, 0.0f), this.character.transform.position);
+
+            // Create a ray from the screen to world.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Cast the ray against the plane.
+            float rayDistance = 0.0f;
+
+            if(plane.Raycast(ray, out rayDistance))
+            {
+                // Calculate ray direction from character's position.
+                Vector3 position = ray.GetPoint(rayDistance);
+                Vector3 direction = position - this.character.transform.position;
+                direction.Normalize();
+
+                // Move with the controlled character.
+                this.character.Aim(direction);
+            }
+        }
     }
 }
