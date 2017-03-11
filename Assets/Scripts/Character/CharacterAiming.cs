586f441a7ee627945fbfb8552cce2139e28e4f37
Piotr Doan <doanpiotr@gmail.com>
2017-03-11 12:47:52 +0100

    Implement smooth IK weight transition.

--- Assets/Scripts/Character/CharacterAiming.cs
+++ Assets/Scripts/Character/CharacterAiming.cs
@@ -6,8 +6,15 @@ public class CharacterAiming : StateMachineBehaviour
 {
     private readonly int HashAimingX = Animator.StringToHash("Aiming X");
     private readonly int HashAimingZ = Animator.StringToHash("Aiming Z");
+    private readonly int HashLookWeight = Animator.StringToHash("Look Weight");
 
-    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Set smooth weight transition.
+        animator.SetFloat(this.HashLookWeight, 1.0f, 0.15f, Time.deltaTime);
+    }
+
+    public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         // Apply rotation based on aiming direction.
         Vector3 direction = new Vector3(animator.GetFloat(HashAimingX), 0.0f, animator.GetFloat(HashAimingZ));
@@ -18,7 +25,7 @@ public class CharacterAiming : StateMachineBehaviour
         }
     }
 
-    override public void OnStateIK(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    public override void OnStateIK(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
         // Create a look at extension in front of the character.
         // Initial vector needs tweaking in order to make a particular animation aim in a correct direction.
@@ -27,7 +34,8 @@ public class CharacterAiming : StateMachineBehaviour
         lookAtExtension = lookAtExtension + animator.transform.position;
 
         // Set inverse kinematic weight and position.
-        animator.SetLookAtWeight(1.0f, 1.0f, 0.9f, 1.0f, 1.0f);
+        float lookWeight = animator.GetFloat(this.HashLookWeight);
+        animator.SetLookAtWeight(lookWeight, 1.0f, 0.25f, 1.0f, 1.0f);
         animator.SetLookAtPosition(lookAtExtension);
     }
 }
---
+++ Assets/Scripts/Character/CharacterRelaxed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRelaxed : StateMachineBehaviour
+{
+    private readonly int HashLookWeight = Animator.StringToHash("Look Weight");
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Set smooth weight transition.
+        animator.SetFloat(this.HashLookWeight, 0.0f, 0.1f, Time.deltaTime);
+    }
+
+    public override void OnStateIK(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // Set inverse kinematic weight and position.
+        float lookWeight = animator.GetFloat(this.HashLookWeight);
+        animator.SetLookAtWeight(lookWeight, 1.0f, 0.25f, 1.0f, 1.0f);
+    }
+}
