a9e697b0e37ae227a222e030844cea5b641b2b7b
Piotr Doan <doanpiotr@gmail.com>
2017-03-19 14:32:05 +0100

    Add a blended tree to the strafing and aiming state.

---
+++ Assets/Scripts/Character/CharacterStanding.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStanding : StateMachineBehaviour
+{
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Set smooth movement fade outtransition.
+        animator.SetFloat(CharacterHash.MovementX, 0.0f, 0.15f, Time.deltaTime);
+        animator.SetFloat(CharacterHash.MovementZ, 0.0f, 0.15f, Time.deltaTime);
+    }
+}
