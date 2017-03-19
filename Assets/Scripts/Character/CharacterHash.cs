66e51c3d643f14779eaaae092e975473c1ee0f75
Piotr Doan <doanpiotr@gmail.com>
2017-03-19 13:16:48 +0100

    Add transition from runnign state to strafing.

--- Assets/Scripts/Character/CharacterHash.cs
+++ Assets/Scripts/Character/CharacterHash.cs
@@ -8,6 +8,8 @@ class CharacterHash
     public static readonly int Running = Animator.StringToHash("Running");
     public static readonly int MovementX = Animator.StringToHash("Movement X");
     public static readonly int MovementZ = Animator.StringToHash("Movement Z");
+    public static readonly int StrafingX = Animator.StringToHash("Strafing X");
+    public static readonly int StrafingZ = Animator.StringToHash("Strafing Z");
 
     public static readonly int Aiming = Animator.StringToHash("Aiming");
     public static readonly int AimingX = Animator.StringToHash("Aiming X");
