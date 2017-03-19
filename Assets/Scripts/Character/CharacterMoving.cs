da365f4e6d37f2bf69954d90caeb072c9ca2c326
Piotr Doan <doanpiotr@gmail.com>
2017-03-19 10:54:54 +0100

    Smoothly calculate the character rotation.

--- Assets/Scripts/Character/CharacterMoving.cs
+++ Assets/Scripts/Character/CharacterMoving.cs
@@ -6,12 +6,18 @@ public class CharacterMoving : StateMachineBehaviour
 {
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // Apply rotation based on movement direction.
-        Vector3 direction = new Vector3(animator.GetFloat(CharacterHash.MovementX), 0.0f, animator.GetFloat(CharacterHash.MovementZ));
+        // Calculate desired direction and rotation factor.
+        Vector3 desiredDirection = new Vector3(animator.GetFloat(CharacterHash.MovementX), 0.0f, animator.GetFloat(CharacterHash.MovementZ));
 
-        if(direction != Vector3.zero)
+        float degreesDifference = Vector3.Angle(animator.transform.forward, desiredDirection);
+        float rotationFactor = 6.0f * Mathf.Clamp(degreesDifference / 40.0f, 0.1f, 1.0f);
+
+        // Apply rotation based on calculated direction.
+        Vector3 calculatedDirection = Vector3.RotateTowards(animator.transform.forward, desiredDirection, rotationFactor * Time.deltaTime, 0.0f);
+
+        if(calculatedDirection != Vector3.zero)
         {
-            animator.transform.rotation = Quaternion.LookRotation(direction);
+            animator.transform.rotation = Quaternion.LookRotation(calculatedDirection);
         }
     }
 }
