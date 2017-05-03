b1a4af5ca527d780893873fcd5f7f5836253fe5a
Piotr Doan <doanpiotr@gmail.com>
2017-05-03 16:30:00 +0200

    Fix delays between moving and aiming transitions.

--- Assets/Scripts/Character/CharacterAiming.cs
+++ Assets/Scripts/Character/CharacterAiming.cs
@@ -18,18 +18,21 @@ public class CharacterAiming : StateMachineBehaviour
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // Calculate desired direction and rotation factor.
-        Vector3 desiredDirection = new Vector3(animator.GetFloat(CharacterHashes.AimingX), 0.0f, animator.GetFloat(CharacterHashes.AimingZ));
-
-        float degreesDifference = Vector3.Angle(animator.transform.forward, desiredDirection);
-        float rotationFactor = 6.0f * Mathf.Clamp(degreesDifference / 40.0f, 0.1f, 1.0f);
-
-        // Apply rotation based on calculated direction.
-        Vector3 calculatedDirection = Vector3.RotateTowards(animator.transform.forward, desiredDirection, rotationFactor * Time.deltaTime, 0.0f);
-
-        if(calculatedDirection != Vector3.zero)
+        if(animator.GetBool(CharacterHashes.Aiming))
         {
-            animator.transform.rotation = Quaternion.LookRotation(calculatedDirection);
+            // Calculate desired direction and rotation factor.
+            Vector3 desiredDirection = new Vector3(animator.GetFloat(CharacterHashes.AimingX), 0.0f, animator.GetFloat(CharacterHashes.AimingZ));
+
+            float degreesDifference = Vector3.Angle(animator.transform.forward, desiredDirection);
+            float rotationFactor = 6.0f * Mathf.Clamp(degreesDifference / 40.0f, 0.1f, 1.0f);
+
+            // Apply rotation based on calculated direction.
+            Vector3 calculatedDirection = Vector3.RotateTowards(animator.transform.forward, desiredDirection, rotationFactor * Time.deltaTime, 0.0f);
+
+            if(calculatedDirection != Vector3.zero)
+            {
+                animator.transform.rotation = Quaternion.LookRotation(calculatedDirection);
+            }
         }
 
         // Calculate rotation for strafing direction in the character's local space.
--- Assets/Scripts/Character/CharacterMoving.cs
+++ Assets/Scripts/Character/CharacterMoving.cs
@@ -6,18 +6,21 @@ public class CharacterMoving : StateMachineBehaviour
 {
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // Calculate desired direction and rotation factor.
-        Vector3 desiredDirection = new Vector3(animator.GetFloat(CharacterHashes.MovementX), 0.0f, animator.GetFloat(CharacterHashes.MovementZ));
-
-        float degreesDifference = Vector3.Angle(animator.transform.forward, desiredDirection);
-        float rotationFactor = 6.0f * Mathf.Clamp(degreesDifference / 40.0f, 0.1f, 1.0f);
-
-        // Apply rotation based on calculated direction.
-        Vector3 calculatedDirection = Vector3.RotateTowards(animator.transform.forward, desiredDirection, rotationFactor * Time.deltaTime, 0.0f);
-
-        if(calculatedDirection != Vector3.zero)
+        if(!animator.GetBool(CharacterHashes.Aiming))
         {
-            animator.transform.rotation = Quaternion.LookRotation(calculatedDirection);
+            // Calculate desired direction and rotation factor.
+            Vector3 desiredDirection = new Vector3(animator.GetFloat(CharacterHashes.MovementX), 0.0f, animator.GetFloat(CharacterHashes.MovementZ));
+
+            float degreesDifference = Vector3.Angle(animator.transform.forward, desiredDirection);
+            float rotationFactor = 6.0f * Mathf.Clamp(degreesDifference / 40.0f, 0.1f, 1.0f);
+
+            // Apply rotation based on calculated direction.
+            Vector3 calculatedDirection = Vector3.RotateTowards(animator.transform.forward, desiredDirection, rotationFactor * Time.deltaTime, 0.0f);
+
+            if(calculatedDirection != Vector3.zero)
+            {
+                animator.transform.rotation = Quaternion.LookRotation(calculatedDirection);
+            }
         }
     }
 }
